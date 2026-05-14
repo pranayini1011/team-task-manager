1a745cbb42b55ccb34535b86acd16f011f3c0335
Sai Pranayini Pendyala <spendyal9@gitam.in>
2026-05-14 22:25:31 +0530

    Updated production API URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000"
+  baseURL: "https://team-task-manager-production-ef53.up.railway.app/docs"
 });
 
 API.interceptors.request.use((req) => {
