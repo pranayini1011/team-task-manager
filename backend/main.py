469cb7aba0f8d59f190d601cb239588acfaf5feb
Sai Pranayini Pendyala <spendyal9@gitam.in>
2026-05-14 22:33:59 +0530

    Fixed production CORS

--- backend/main.py
+++ backend/main.py
@@ -15,9 +15,16 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+
+    allow_origins=[
+        "http://localhost:3000",
+        "https://team-task-manager-neon-six.vercel.app"
+    ],
+
     allow_credentials=True,
+
     allow_methods=["*"],
+
     allow_headers=["*"],
 )
 
