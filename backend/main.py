8821732fe4ae68778810a9885edd9b22d886c00a
Sai Pranayini Pendyala <spendyal9@gitam.in>
2026-05-14 22:46:38 +0530

    Fixed main.py syntax error

--- backend/main.py
+++ backend/main.py
@@ -41,47 +41,4 @@ app.include_router(project_router)
 
 app.include_router(task_router)
 
-app.include_router(dashboard_router)from fastapi import FastAPI
-
-from fastapi.middleware.cors import CORSMiddleware
-
-from database import Base, engine
-
-from routes.users import router as user_router
-from routes.projects import router as project_router
-from routes.tasks import router as task_router
-from routes.dashboard import router as dashboard_router
-
-Base.metadata.create_all(bind=engine)
-
-app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-
-    allow_origins=[
-        "http://localhost:3000",
-        "https://team-task-manager-neon-six.vercel.app"
-    ],
-
-    allow_credentials=True,
-
-    allow_methods=["*"],
-
-    allow_headers=["*"],
-)
-
-@app.get("/")
-def home():
-
-    return {
-        "message": "Team Task Manager API Running"
-    }
-
-app.include_router(user_router)
-
-app.include_router(project_router)
-
-app.include_router(task_router)
-
 app.include_router(dashboard_router)
